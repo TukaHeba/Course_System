f21b6297da91b4c87b95f2f9c706a90ada0dcac7
Tuka Heba <tuka.heba@gmail.com>
2024-10-05 07:44:36 +0300

    Add Migrations

--- database/migrations/2024_09_28_014101_create_course_instructor_table.php
+++ database/migrations/2024_09_28_014101_create_course_instructor_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('course_instructor', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('type', ['admin', 'instructor', 'student']);
-            $table->rememberToken();
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->foreignId('instructor_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('course_instructor');
     }
 };
