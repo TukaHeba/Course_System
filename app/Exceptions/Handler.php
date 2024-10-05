b52b73695c2962928b8796b76a7839c3a88ec8e1
Tuka Heba <tuka.heba@gmail.com>
2024-10-05 07:48:13 +0300

    Handling ModelNotFoundException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Services\ApiResponseService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +28,23 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        // Check if the exception is an instance of ModelNotFoundException
+        if ($exception instanceof ModelNotFoundException) {
+            return ApiResponseService::error(null, 'Model not found', 404);
+        }
+
+        // For other exceptions, call the parent render method
+        return parent::render($request, $exception);
     }
 }
