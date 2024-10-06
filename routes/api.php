ab65513a8be3e7f768e4ecf8a2e8bb990198dbb1
Tuka Heba <tuka.heba@gmail.com>
2024-10-07 01:23:55 +0300

    Routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\InstructorController;
+use App\Http\Controllers\Api\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+///===== Course Routes =====///
+Route::controller(CourseController::class)->group(function () {
+    // List all students
+    Route::get('/courses/{id}/students',  'listStudents');
+
+    // List all instructors
+    Route::get('/courses/{id}/instructors',  'listInstructors');
+
+    // Assign instructors
+    Route::post('/courses/{id}/instructors',  'assignInstructors');
 });
+Route::apiResource('courses', CourseController::class);
+
+
+///===== Instructor Routes =====///
+Route::controller(InstructorController::class)->group(function () {
+    // List all students
+    Route::get('/instructors/{id}/students', 'listStudents');
+
+    // List all courses
+    Route::get('/instructors/{id}/courses', 'listCourses');
+
+    // Assign courses
+    Route::post('/instructors/{id}/courses', 'assignCourses');
+});
+Route::apiResource('instructors', InstructorController::class);
+
+
+///===== Student Routes =====///
+Route::controller(StudentController::class)->group(function () {
+    // List all courses
+    Route::get('/students/{id}/courses', 'listCourses');
+
+    // Register in courses
+    Route::post('/student/{id}/courses', 'registerInCourses');
+});
+Route::apiResource('students', StudentController::class);
